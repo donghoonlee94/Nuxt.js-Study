c24e2ffb47d56ef539d2f02c9b0c3ef42fd79fe3
donghoonlee94 <94donghoon.lee@gmail.com>
2020-10-30 16:42:36 +0900

    :sparkles: Add pages

---
+++ pages/users.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>users.vue</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/users/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>_id</div>
+</template>
+
+<script>
+export default {
+  // validate({ params }) {
+  //   // Must be a number
+  //   console.log(params)
+  //   return /^\d+$/.test(params.id)
+  // },
+}
+</script>
+
+<style></style>
